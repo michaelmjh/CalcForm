5f2aaeeb887ea47a3578d8911dff7a21f24b9819
michaelmjh <michaelmjh@icloud.com>
2021-01-13 11:22:41 +0000

    Changes

---
+++ CalcForm/CalcBrain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcForm
+{
+    class CalcBrain
+    {
+        float firstNumber;
+        string operationToPerform;
+
+        public void storeFirstNumber(float numberToStore)
+        {
+            firstNumber = numberToStore;
+        }
+
+        public void storeOperationToDo(string operationToStore)
+        {
+            operationToPerform = operationToStore;
+        }
+
+        public string performCalculation(float secondNumber)
+        {
+            float answer = 0;
+            if (operationToPerform == "+")
+            {
+                answer = firstNumber + secondNumber;
+            }
+            else if (operationToPerform == "-")
+            {
+                answer = firstNumber - secondNumber;
+            }
+            else if (operationToPerform == "*")
+            {
+                answer = firstNumber * secondNumber;
+            }
+            else  if (operationToPerform == "/")
+            {
+                answer = firstNumber / secondNumber;
+            }
+            return answer.ToString();
+        }
+    }
+}
